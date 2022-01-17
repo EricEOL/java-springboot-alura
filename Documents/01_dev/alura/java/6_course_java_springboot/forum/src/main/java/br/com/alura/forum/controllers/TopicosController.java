95860566870388d9580a56169d37b04255a5fce3
EricEOL <lopes.eric051@gmail.com>
2022-01-17 19:29:50 -0300

    Criando um DTO para devolver somente os atributos desejados do objeto Tópico

--- Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -1,29 +1,24 @@
 package br.com.alura.forum.controllers;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import br.com.alura.forum.controllers.DTO.TopicoDTO;
 import br.com.alura.forum.models.Curso;
 import br.com.alura.forum.models.Topico;
 
-@Controller
+@RestController
 public class TopicosController {
 
 	@RequestMapping("/topicos")
-	@ResponseBody
-	public List<Topico> listar() {
+	public List<TopicoDTO> listar() {
 
 		Topico topico = new Topico("Dúvida Java", "Não consigo sei lá oque", new Curso("Java", "Programação"));
 		
-		/*List<Topico> topicos = new ArrayList<Topico>();
-		topicos.add(topico);*/
-
-		return Arrays.asList(topico, topico, topico);
+		return TopicoDTO.converter(Arrays.asList(topico, topico, topico, topico));
 
 	}
 
