99e35030bb51156bda2fe63c211b786657d74b04
EricEOL <lopes.eric051@gmail.com>
2022-01-20 21:52:27 -0300

    Rota para alteração de um tópico já cadastrado

--- Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -3,13 +3,16 @@ package br.com.alura.forum.controllers;
 import java.net.URI;
 import java.util.List;
 
+import javax.transaction.Transactional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 
 import br.com.alura.forum.controllers.DTO.DetailsTopicoDTO;
 import br.com.alura.forum.controllers.DTO.TopicoDTO;
+import br.com.alura.forum.controllers.form.AttTopicoForm;
 import br.com.alura.forum.controllers.form.TopicoForm;
 import br.com.alura.forum.models.Topico;
 import br.com.alura.forum.repositories.CursoRepository;
@@ -69,6 +73,14 @@ public class TopicosController {
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
 	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AttTopicoForm form) {
+		
+		Topico topico = form.atualizar(id, topicoRepository);
+
+		return ResponseEntity.ok(new TopicoDTO(topico));
+	}
 
 
 }
