2566e1111d8bcbff1b079e61bd74e07cc31cd7c8
EricEOL <lopes.eric051@gmail.com>
2022-01-22 18:31:57 -0300

    Preparando as classes de usuário e perfil para que sejam utilizadas na autenticação

--- Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -23,7 +23,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
 		.antMatchers(HttpMethod.GET ,"/topicos").permitAll()
-		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+		.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+		.anyRequest().authenticated()
+		.and().formLogin();
 	}
 	//
 	// Configurações de recursos estáticos(js, css, imagens entre outros)
