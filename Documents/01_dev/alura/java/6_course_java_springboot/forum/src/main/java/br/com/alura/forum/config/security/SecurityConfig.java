50d44b2e785681393507660fd074fe37f3548530
EricEOL <lopes.eric051@gmail.com>
2022-01-22 19:21:36 -0300

    Configurando a lógica de autenticação de usuário para acessar rotas restritas

--- Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ Documents/01_dev/alura/java/6_course_java_springboot/forum/src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,15 +8,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private AuthenticationService authenticationService;
+	
 	// Configurações de autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		
+		auth.userDetailsService(authenticationService).passwordEncoder(new BCryptPasswordEncoder());
+	
 	}
 	
 	// Configurações de autorização
@@ -33,5 +40,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		
 	}
-	
+
 }
